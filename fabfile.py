2f79dd09149928fd81112cb41d47bc0718fa8dcc
Yosiya Hinosawa <stibium121@gmail.com>
2013-05-15 22:34:08 +0900

    added fabfile.py.

---
+++ fabfile.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+from fabric.api import local, run, cd, env
+from fabric.colors import green
+
+env.use_ssh_config = True
+env.shell = '/bin/sh -c'
+
+env.hosts = ['sakura']
+
+def deploy():
+
+  print(green('Deploying...'))
+
+  with cd('www/imageroundr'):
+    run('git pull')
+
+  print(green('Done.'))
+
+  local('rm fabfile.pyc')
